a9e0f5c69502e0cb0c749cdfab57fca5fae6a63a
tgtiburon <tg.tiburon@gmail.com>
2021-12-06 14:42:40 -0500

    post is working

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
+const exp = require('constants');
 const express = require('express');
+const fs = require('fs');
+// path is built into node.js API that makes working
+// with file systems more predictable.
+const path = require('path');  
 
 const { animals }  = require('./data/animals');
 
@@ -23,6 +28,21 @@ const app = express();
 // 2 arguments, 1. string route the client will have to fetch from, 2. callback function that will
 // execute each time that route is accesset with GET request.
 
+
+// app.use() executed by our express.js server that mounts a function
+// to the server that our requests will pass through before getting 
+// to the intended endpoint.
+// MIDDLEWARE
+// parse incoming string or array data
+// express.urlendcoded is builtin express.js. It takes POST data and converts it to
+// key/value pairings that can be accessed in req.body.  
+// Extended true means it has nested data so it needs to keep parsing as
+// deep as it can
+
+// The two below functions must always be set up if you are accepting POST
+app.use(express.urlencoded({ extended : true}));
+// parse incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray)   {
 
@@ -70,6 +90,49 @@ function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
 }
+
+
+function createNewAnimal(body, animalsArray) {
+
+    const animal = body;
+    animalsArray.push(animal);
+    // writeFileSync does not need callbacks, good for small files
+    // _dirname is the directory our program is run from.
+    
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        // null = don't edit any of our data
+        // 2 means put white spaces between values to make it more readable.
+        JSON.stringify({animals: animalsArray }, null, 2)
+    );
+
+    
+    return animal;
+}
+/**
+ *  Make sure each key is present and the correct type
+ * @param {*} animal 
+ */
+
+function validateAnimal(animal) {
+
+    if(!animal.name || typeof animal.name !=='string') {
+        return false;
+    }
+    if(!animal.species || typeof animal.species !=='string'){
+        return false;
+    }
+    if(!animal.diet || typeof animal.diet !== 'string') {
+
+        return false;
+    }
+    if(!animal.personalityTraits || !Array.isArray(animal.personalityTraits)){
+        return false;
+    }
+
+    return true;
+
+}
 app.get('/api/animals', (req, res) => {
     // we are using the send method from the response to send a string
     //res.send('Hello!');
@@ -95,6 +158,37 @@ app.get('/api/animals/:id', (req, res)=> {
     }
     
 });
+
+// creates a route that listens for post requests
+// we want the server to accept data from the user
+app.post('/api/animals', (req, res) => {
+    // req.body is where our incoming content will be 
+    // set id based on what the index of the animalarr will be
+    req.body.id = animals.length.toString();
+
+    // if any data in req.body is incorrrect, send 400 error back
+    if(!validateAnimal(req.body)){
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+
+         // add animal to json file and animals array
+    const animal = createNewAnimal(req.body, animals);
+
+    res.json(req.body);
+
+
+
+    }
+
+   
+});
+
+
+
+
+
+
+
 
 app.listen(PORT, ()=> {
     console.log(`API server now on port ${PORT}!`);
